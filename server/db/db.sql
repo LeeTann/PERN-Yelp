6444f08ff34c88bf5eb95b41974bdad646d75546
ltann <leeroyleeroy18@gmail.com>
2020-09-02 22:41:40 -0700

    Added reviews table

--- server/db/db.sql
+++ server/db/db.sql
@@ -5,7 +5,6 @@ CREATE TABLE restaurants (
   price_range INT NOT NULL CHECK(price_range >=1 and price_range <=5)
 );
 
-
 // Crud operations
 SELECT * FROM restaurants;
 
@@ -16,3 +15,17 @@ INSERT INTO restaurants (name, location, price_range) VALUES ('koon thai', 'kear
 UPDATE restaurants SET name = 'red lobster', location = 'oceanside', price_range = 5 WHERE id = 4 RETURNING *;
 
 DELETE FROM restaurants where id = 1;
+
+CREATE TABLE reviews (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+  name VARCHAR(50) NOT NULL,
+  review TEXT NOT NULL,
+  rating INT NOT NULL CHECK(rating >=1 and rating <=5)
+);
+
+DROP TABLE reviews;
+
+INSERT INTO reviews (restaurant_id, name, review, rating) VALUES ('11', 'carl', 'restaurant was good', 5);
+
+SELECT * FROM reviews WHERE restaurant_id = 11;
